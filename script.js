45459f97662d20ee592d30f5a1b8f8b86de47e6a
Adenny Fernandes <tabuadamultiplicao@gmail.com>
2022-07-01 20:23:02 -0300

    up

---
+++ script.js
@@ -0,0 +1,8 @@
+function Calcular() {
+    var num1 = document.querySelector(".num1").value;
+    var num2 = document.querySelector(".num2").value;
+
+    var resultado = parseInt(num1) * parseInt(num2);
+    document.querySelector(".resultado").innerHTML = resultado;
+
+}
